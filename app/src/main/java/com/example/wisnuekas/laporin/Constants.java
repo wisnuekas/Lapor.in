51a998e6459b251007c006e9bc76e7e941e83868
wisnuekas <wisnuekas@Wisnus-MacBook-Air.local>
2017-06-04 20:30:52 +0700

    file constrant untuk memanggil fungsi

---
+++ app/src/main/java/com/example/wisnuekas/laporin/Constants.java
@@ -0,0 +1,10 @@
+package com.example.wisnuekas.laporin;
+
+/**
+ * Created by wisnuekas on 5/26/17.
+ */
+
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+}
